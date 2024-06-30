4234202a66335199bee2d5eeedd568f23ab45b01
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-06-30 23:05:59 +0800

    feat: add zero padding

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+
+
 function getTime() {
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
+    const padHours = String(hours).padStart(2, "0");
+    const padMinutes = String(minutes).padStart(2, "0");
+    const padSeconds = String(seconds).padStart(2, "0");
     const time = document.getElementById("time");
-    time.textContent = `${hours}:${minutes}:${seconds}`;
+    time.textContent = `${padHours} : ${padMinutes} : ${padSeconds}`;
 }
 
 setInterval(getTime, 1000);
